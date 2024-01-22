80b3edf9038c6760e9a6774e14c0cabd46ee29f2
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-01-22 20:42:27 +0530

    feat: module wise flow runner complete

--- flow/flow.py
+++ flow/flow.py
@@ -1,14 +1,15 @@
 from os import path
 
-from pyflow.flow_runner import FlowRunner
+from pyflow import PPARunner
 from platforms.sky130hd.config import SKY130HD_PLATFORM_CONFIG
 
-gcd_runner = FlowRunner(
+gcd_runner = PPARunner(
+	design_name="gcd",
 	global_flow_config={
 		**SKY130HD_PLATFORM_CONFIG,
-		'DESIGN_NAME': 'gcd',
 		'PLATFORM': 'sky130hd',
 		'VERILOG_FILES': [path.join('designs', 'src', 'gcd', 'gcd.v')],
+		'DESIGN_DIR': path.join('designs', 'sky130hd', 'gcd'),
 		'YOSYS_CMD': '/usr/bin/miniconda3/bin/yosys',
 		'OPENROAD_CMD': '/usr/bin/miniconda3/bin/openroad',
 		'KLAYOUT_CMD': 'klayout',
@@ -16,7 +17,7 @@ gcd_runner = FlowRunner(
 	},
 	modules=[
 		{
-			'name': 'gcd',
+			'name': 'GcdUnitCtrlRTL_0x4d0fc71ead8d3d9e',
 			'flow_config': {
 				'CORE_UTILIZATION': {
 					'start': 10,
@@ -29,8 +30,18 @@ gcd_runner = FlowRunner(
 				'array_size': [10, 20, 30],
 				'temp': {'start': 1, 'end': 50, 'step': 2}
 			}
+		},
+		{
+			'name': 'RegRst_0x9f365fdf6c8998a',
+			'flow_config': {},
+			'parameters': {}
+		},
+		{
+			'name': 'gcd',
+			'flow_config': {},
+			'parameters': {}
 		}
 	]
 )
 
-gcd_runner.run_flow()
+gcd_runner.run_ppa_analysis()
---
+++ flow/pyflow/ppa_runner.py
@@ -0,0 +1,48 @@
+from typing import TypedDict, Union, Any, Optional
+from os import path
+from shutil import rmtree
+
+from .flow_config import FlowConfig, FlowConfigDict
+from .flow_steps import preprocess, synth, floorplan
+
+class ParameterSweepDict(TypedDict):
+	start: float
+	end: float
+	step: float
+
+class Module(TypedDict):
+	name: str
+	parameters: dict[str, Union[ParameterSweepDict, list[Any], Any]]
+	flow_config: dict[str, Union[ParameterSweepDict, list[Any], Any]]
+
+class PPARunner:
+	design_name: str
+	global_flow_config: FlowConfigDict
+	modules: list[Module]
+	work_home: str
+
+	def __init__(self, design_name: str, global_flow_config: FlowConfigDict, modules: list[Module], work_home: Optional[str] = None):
+		self.design_name = design_name
+		self.global_flow_config = global_flow_config
+		self.modules = modules
+		self.work_home = work_home if work_home != None else path.abspath(path.join('.', 'runs', design_name))
+
+	def run_ppa_analysis(self):
+		for module in self.modules:
+			print(f"Running flow for module {module['name']}")
+
+			module_work_home = path.join(self.work_home, module['name'])
+			module_config: FlowConfig = FlowConfig({
+				**self.global_flow_config,
+				'DESIGN_NAME': module['name'],
+				'WORK_HOME': module_work_home
+			})
+
+			if path.exists(module_work_home):
+				rmtree(module_work_home)
+
+			module_config = preprocess(module_config)
+			synth(module_config)
+
+	def clean_runs(self):
+		rmtree(self.global_flow_config.get('WORK_HOME'))
